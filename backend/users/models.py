912a2edd3f020f8000d7254a6036d32ab4201f67
Maxim <mgraf@yandex.ru>
2022-09-16 18:28:00 +0300

    add users/models

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,77 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    USER = 'user'
+    ADMIN = 'admin'
+
+    USER_ROLE_CHOICES = (
+        (USER, 'user'),
+        (ADMIN, 'administrator'),
+    )
+
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        verbose_name='Адрес электронной почты',
+    )
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name='Уникальный юзернейм',
+    )
+    first_name = models.CharField(
+        max_length=150,
+        blank=True,
+        verbose_name='Имя',
+    )
+    last_name = models.CharField(
+        max_length=150,
+        blank=True,
+        verbose_name='Фамилия',
+    )
+    password = models.CharField(
+        max_length=150,
+        verbose_name='Пароль'
+    )
+        
+    is_subscribed = models.BooleanField(
+        default=False,
+        verbose_name='Подписан ли текущий пользователь на этого',
+    )
+    role = models.CharField(
+        'Пользовательская роль',
+        max_length=15,
+        choices=USER_ROLE_CHOICES,
+        default=USER
+    )
+
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN or self.is_superuser
+
+    def __str__(self):
+        return self.username
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Пользователь',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор',
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self):
+        return f'{self.user} {self.author}'
