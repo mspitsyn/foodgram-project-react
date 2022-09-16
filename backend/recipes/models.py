7b610ffed7d4952195033a4c69f6f81d7eac9ed6
Maxim <mgraf@yandex.ru>
2022-09-16 19:18:34 +0300

    add base model in recipes

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,94 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        verbose_name='Название ингридиента',
+        max_length=200,
+    )
+    quantity = models.PositiveIntegerField(verbose_name="Количество")
+    measurement_unit = models.CharField(
+        max_length=200,
+        verbose_name='Единицы измерения',
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        verbose_name='Название тега',
+        max_length=200
+    )
+    slug = models.SlugField(
+        verbose_name='Слаг',
+        max_length=200,
+        unique=True,
+    )
+    color = models.CharField(
+        verbose_name='Цветовой HEX-код',
+        max_length=7
+    )
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор публикации',
+    )
+    name = models.CharField(
+        verbose_name='Название рецепта',
+        max_length=200
+    )
+    image = models.ImageField(
+        'Картинка',
+        upload_to='recipes/',
+        blank=True,
+    )
+    text = models.TextField(
+        verbose_name='Описание рецепта'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='IngredientMount',
+        related_name='ingredients',
+        verbose_name='Ингредиенты',
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        through='TagRecipe',
+        related_name='tags',
+        verbose_name='Тэг',
+    )
+    cooking_time = models.IntegerField(
+        validators=(MinValueValidator(1),),
+        verbose_name='Время приготовления',
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
