81cfc8a1154877165691f58f709132b4494dc820
Maxim <mgraf@yandex.ru>
2022-10-09 22:34:34 +0300

    after review 2.2

--- backend/api/services.py
+++ backend/api/services.py
@@ -4,15 +4,17 @@ from recipes.models import IngredientAmount
 
 
 def get_list_ingridients(user):
-    ingredients = IngredientAmount.objects.filter(
+    shopping_list = IngredientAmount.objects.filter(
         recipe__cart__user=user
     ).values(
-        name=F('ingredient__name'),
+        name=F('ingredients__name'),
         measurement_unit=F('ingredients__measurement_unit')
-    ).annotate(total=Sum('amount')).values_list(
-        'ingredient__name', 'total', 'ingredients__measurement_unit'
+    ).annotate(amount=Sum('amount')).values_list(
+        'ingredients__name', 'amount', 'ingredients__measurement_unit'
     )
-    shopping_cart = ''.join(
-        f'{key} - {value} - {unit}\n' for key, value, unit in ingredients
-    )
+    shopping_cart = '\n'.join([
+        f'{ingredient["ingredients__name"]} - {ingredient["total"]} '
+        f'{ingredient["ingredients__measurement_unit"]}'
+        for ingredient in shopping_list
+    ])
     return shopping_cart
