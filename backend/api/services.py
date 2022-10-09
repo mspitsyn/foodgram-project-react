99900c1384883d078b22e4c2824d0505c29f36bf
Maxim <mgraf@yandex.ru>
2022-10-09 23:09:51 +0300

    after review 2.3

--- backend/api/services.py
+++ backend/api/services.py
@@ -1,4 +1,6 @@
 from django.db.models import F, Sum
+from django.template.loader import render_to_string
+from weasyprint import HTML
 
 from recipes.models import IngredientAmount
 
@@ -9,12 +11,11 @@ def get_list_ingridients(user):
     ).values(
         name=F('ingredients__name'),
         measurement_unit=F('ingredients__measurement_unit')
-    ).annotate(total=Sum('amount')).values_list(
-        'ingredients__name', 'total', 'ingredients__measurement_unit'
+    ).annotate(amount=Sum('amount')).values_list(
+        'ingredients__name', 'amount', 'ingredients__measurement_unit'
     )
-    shopping_cart = '\n'.join([
-        f'{ingredient["ingredients__name"]} - {ingredient["total"]} '
-        f'{ingredient["ingredients__measurement_unit"]}'
-        for ingredient in shopping_list
-    ])
-    return shopping_cart
+    shopping_cart = render_to_string('recipes/pdf_template.html',
+                                     {'ingredients': shopping_list})
+    html = HTML(string=shopping_cart)
+    result = html.write_pdf()
+    return result
