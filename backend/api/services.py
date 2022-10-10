a0fe09f92647e0b3e2bc0ca7435201008709ae18
Maxim <mgraf@yandex.ru>
2022-10-10 08:51:41 +0300

    after review 2.5

--- backend/api/services.py
+++ backend/api/services.py
@@ -9,8 +9,10 @@ def get_list_ingridients(user):
     ).values(
         name=F('ingredients__name'),
         measurement_unit=F('ingredients__measurement_unit')
-    ).annotate(amount=Sum('amount')).order_by()
-    shopping_cart = ''.join(
-        f'{key} - {value} - {unit}\n' for key, value, unit in ingredients
-    )
+    ).annotate(amount=Sum('amount')).order_by
+    shopping_cart = '\n'.join([
+            f'{ingredient["name"]} - {ingredient["amount"]} '
+            f'{ingredient["measurement_unit"]}'
+            for ingredient in ingredients
+    ])
     return shopping_cart
